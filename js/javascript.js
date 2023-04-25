05395e83bff9533fe16394b9ac253bc456213e57
Luka Jincharadze <lukajinchara@gmail.com>
2023-04-25 10:18:53 +0400

    update

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,27 @@
 let navBarLinks = document.getElementById("navbarLinks");
 let toggleButton = document.getElementById("toggle-button");
-let navli = document.getElementById("nav-li");
-let navulSelector = document.getElementById("nav-ul-selector");
+let navUl = document.getElementById("nav-ul-selector");
 
-  
 toggleButton.addEventListener("click", function() {
     navBarLinks.classList.toggle("active");
-    // navli.style.marginLeft = '0';
 
-    let navulSelector = navulSelector.querySelectorAll("li");
-    for (let i = 0; i < lis.length; i++) {
-        lis[i].style.marginLeft = '0';
+    if (navBarLinks.classList.contains("active")) {
+        let lis = navUl.querySelectorAll("li");
+        for (let i = 0; i < lis.length; i++) {
+            lis[i].style.marginLeft = '0';
+        }
     }
 });
 
+function checkScreenWidth() {
+    if (window.innerWidth >= 1000) {
+      let lis = navUl.querySelectorAll("li");
+      for (let i = 0; i < lis.length; i++) {
+        lis[i].style.marginLeft = '50px';
+      }
+    }
+  }
+  
+  // Call checkScreenWidth on load and resize
+  window.addEventListener("load", checkScreenWidth);
+  window.addEventListener("resize", checkScreenWidth);
